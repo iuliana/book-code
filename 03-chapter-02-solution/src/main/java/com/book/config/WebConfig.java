c5398fa49ccf4a98b95dbb6b30c5c7c1681f4aad
Iuliana Cosmina <iuliana.cosmina@gmail.com>
2015-02-19 00:13:30 +0200

    #13 added chapter 3 sources - xml and annotation configuration

---
+++ 03-chapter-02-solution/src/main/java/com/book/config/WebConfig.java
@@ -0,0 +1,42 @@
+package com.book.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * Created by iuliana.grajdeanu on 2/18/15.
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.ok.controllers"})
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    //Declare our static resources. I added cache to the java config but it’s not required.
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**").addResourceLocations("/images/").setCachePeriod(31556926);
+    }
+
+    // <=> <mvc:default-servlet-handler/>
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("welcome");
+    }
+
+    @Bean
+     InternalResourceViewResolver getViewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/");
+        resolver.setSuffix(".jsp" );
+        return resolver;
+    }
+
+}
