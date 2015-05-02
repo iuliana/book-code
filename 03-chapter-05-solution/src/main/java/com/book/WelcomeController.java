d77616c41e7fe90f055e5984ea9863aff0edbfeb
Iuliana Cosmina <iuliana.cosmina@gmail.com>
2015-05-03 00:24:57 +0300

    #27 polished ControllerClassNameHandlerMapping

--- 03-chapter-05-solution/src/main/java/com/book/WelcomeController.java
+++ 03-chapter-05-solution/src/main/java/com/book/WelcomeController.java
@@ -1,23 +1,24 @@
 package com.book;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Created by iuliana.cosmina on 3/18/15.
+ * Created by iuliana.cosmina on 5/2/15.
  */
 @Controller
-public class WelcomeController extends AbstractController {
+public class WelcomeController {
 
-    @Override
-    protected ModelAndView handleRequestInternal(HttpServletRequest arg0,
-                                                 HttpServletResponse arg1) throws Exception {
-        ModelAndView model = new ModelAndView("welcome");
-        model.addObject("message", "Hello to you!");
-        return model;
+    @RequestMapping
+    public String sayhi(Model model){
+        model.addAttribute("salute", "Hi!");
+        return "welcome";
+    }
+
+    @RequestMapping
+    public String sayhello(Model model){
+        model.addAttribute("salute", "Hello!");
+        return "welcome";
     }
 }
--- 03-chapter-05-solution/src/main/webapp/WEB-INF/pages/welcome.jsp
+++ 03-chapter-05-solution/src/main/webapp/WEB-INF/pages/welcome.jsp
@@ -1,10 +1,16 @@
-
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
-    <title>Welcome page</title>
+    <link rel="stylesheet" type="text/css" href="<c:url value='/styles/general.css'/>">
 </head>
+<title>Spring Sample Web Application</title>
 <body>
-${message}
+<table>
+    <tr>
+        <td><img src="<c:url value='/images/banner.png'/>"/></td>
+        <td>${salute}</td>
+    </tr>
+    <tr><td colspan="2"><em><h4>SpringSource Certified Spring Web Application Developer Exam - Sample application</h4></em></td></tr>
+</table>
 </body>
 </html>
