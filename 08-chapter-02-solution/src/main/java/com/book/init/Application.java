2122d2227161396910af0d4c5a2f7e6e9a4cb7d0
Iuliana Cosmina <iuliana.cosmina@gmail.com>
2015-09-27 15:29:25 +0300

    #30 started on spring boot section

---
+++ 08-chapter-02-solution/src/main/java/com/book/init/Application.java
@@ -0,0 +1,17 @@
+package com.book.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.book.init", "com.book.web"})
+public class Application {
+
+    public static void main(String[] args) {
+       SpringApplication.run(Application.class, args);
+    }
+
+}
